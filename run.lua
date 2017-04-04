ad7a4292dffcdab36e3699cc6c804b1b8bab97a6
Christopher Moore <christopher.e.moore@gmail.com>
2017-04-04 14:23:36 +0100

    more changes

--- run.lua
+++ run.lua
@@ -43,17 +43,14 @@ local function copyFileToDir(srcfile,dstdir)
 	end
 end
 
-local function copyDirToDir(srcdir, dstdir)
+-- TODO ignore hidden files, or at least just skip the .git folders
+local function copyDirToDir(srcdir, dstdir, pattern)
+	pattern = pattern or '*'
 	if ffi.os == 'Windows' then
-		-- all files
-		exec('xcopy '..fixpath(srcdir)..' '..fixpath(dstdir..'/'..srcdir)..' /E /I /Y')
-		-- lua files only?
-		--exec('xcopy '..fixpath(srcdir)..'\\*.lua '..fixpath(dstdir..'/'..srcdir)..' /E /I /Y')
+		exec('xcopy '..fixpath(srcdir)..'\\'..pattern..' '..fixpath(dstdir..'/'..srcdir)..' /E /I /Y')
 	else
-		-- all files
-		exec('cp -R '..srcdir..' '..dstdir)
-		-- lua files only?
-		--exec("rsync -avm --include='*.lua' -f 'hide,! */' "..srcdir.." "..dstdir)
+		--exec('cp -R '..srcdir..' '..dstdir)
+		exec("rsync -avm --include='"..pattern.."' -f 'hide,! */' "..srcdir.." "..dstdir)
 	end
 end
 
@@ -72,18 +69,18 @@ local function copyBody(destDir)
 end
 
 -- returns t[plat], t[1], or t, depending on which exists and is a table 
-local function getForPlat(t, plat)
+local function getForPlat(t, plat, reqtype)
 	if not t then return end
 	return t[plat] 
-		or (type(t[1]) == 'table' and t[1] or t)
+		or (type(t[1]) == reqtype and t[1] or t)
 end
 
 local function getLuajitLibs(plat)
-	return getForPlat(luajitLibs, plat)
+	return getForPlat(luajitLibs, plat, 'table')
 end
 
 local function getLuaArgs(plat)
-	return getForPlat(luaArgs, plat)
+	return getForPlat(luaArgs, plat, 'string')
 end
 
 mkdir('dist')
@@ -91,13 +88,12 @@ mkdir('dist')
 -- the windows-specific stuff:
 local function makeWin(arch)
 	assert(arch == 'x86' or arch == 'x64', "expected arch to be x86 or x64")
-	local bits = assert( ({x86='32',x64='64'})[arch], "don't know what bits of windows this is (32? 64? etc?)")
+	local bits = assert( ({x86='32',x64='64'})[arch], "don't know what bits of arch this is (32? 64? etc?)")
 	local osDir = 'dist/win'..bits
 	mkdir(osDir)
 	local runBat = osDir..'/run.bat'
 
 -- TODO for now windows runs with no audio and no editor.  eventually add OpenAL and C/ImGui support. 
-	local winLuaArgs = getLuaArgs'win'
 	file[runBat] = table{
 		'cd data',
 		[[set PATH=%PATH%;bin\Windows\]]..arch,
@@ -105,7 +101,9 @@ local function makeWin(arch)
 	}:append(
 		luaDistVer == 'luajit' and {'set LUAJIT_LIBPATH=.'} or {}
 	):append{
-		[[bin\Windows\x86\]]..luaDistVer..'.exe '..winLuaArgs..' > out.txt 2> err.txt',
+		[[bin\Windows\x86\]]..luaDistVer..'.exe '
+			..(getLuaArgs'win' or '')
+			..' > out.txt 2> err.txt',
 		'cd ..'
 	}:concat'\n'..'\n'
 
@@ -173,10 +171,10 @@ local function makeOSX()
 </dict>
 </plist>]]
 
-	local osxLuaArgs = getLuaArgs'osx'
 	local macOSDir = contentsDir..'/MacOS'
 	mkdir(macOSDir)
 	local runSh = macOSDir..'/run.sh' 
+	
 	file[runSh] = table{
 		[[#!/usr/bin/env bash]],
 		-- https://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
@@ -186,7 +184,9 @@ local function makeOSX()
 	}:append(
 		luaDistVer == 'luajit' and {'export LUAJIT_LIBPATH="."'} or {}
 	):append{
-		'./'..luaDistVer..' '..osxLuaArgs..' > out.txt 2> err.txt',
+		'./'..luaDistVer..' '
+			..(getLuaArgs'osx' or '')
+			..' > out.txt 2> err.txt',
 	}:concat'\n'..'\n'
 	exec('chmod +x '..runSh)
 
@@ -211,7 +211,51 @@ local function makeOSX()
 	end
 end
 
+-- should I include binaries in the linux distribution?
+local function makeLinux(arch)
+	assert(arch == 'x86' or arch == 'x64', "expected arch to be x86 or x64")
+	local bits = assert( ({x86='32',x64='64'})[arch], "don't know what bits of arch this is (32? 64? etc?)")
+	local osDir = 'dist/linux'..bits
+	mkdir(osDir)
+	
+	local runSh = osDir..'/run.sh'
+	
+	file[runSh] = table{
+		[[#!/usr/bin/env bash]],
+		'cd data',
+		[[export LUA_PATH="./?.lua;./?/?.lua"]],
+	}:append(
+		luaDistVer == 'luajit' and {'export LUAJIT_LIBPATH="."'} or {}
+	):append{
+		'bin/Linux/'..arch..'/'..luaDistVer..' '
+			..(getLuaArgs'linux' or '')
+			..' > out.txt 2> err.txt',
+	}:concat'\n'..'\n'
+	exec('chmod +x '..runSh)
+
+	local dataDir = osDir..'/data'
+	mkdir(dataDir)
+	mkdir(dataDir..'/bin')
+	mkdir(dataDir..'/bin/Linux')
+	local binDir = dataDir..'/bin/Linux/'..arch
+	mkdir(binDir)
+	
+	-- copy luajit
+	copyFileToDir('../ufo/bin/Linux/'..arch..'/'..luaDistVer, binDir)
+
+	-- copy body
+	copyBody(dataDir)
+
+	-- copy ffi linux so's
+	local linuxLuajitLibs = getLuajitLibs'linux'
+	if linuxLuajitLibs then
+		for _,fn in ipairs(linuxLuajitLibs) do
+			copyFileToDir('../ufo/bin/Linux/'..arch..'/'..fn..'.so', binDir)
+		end
+	end
+end
 
 if target == 'all' or target == 'osx' then makeOSX() end
 if target == 'all' or target == 'win32' then makeWin('x86') end
 --if target == 'all' or target == 'win64' then makeWin('x64') end -- ufo only runs the 64 bit versions for amd ...
+if target == 'all' or target == 'linux' then makeLinux('x64') end
