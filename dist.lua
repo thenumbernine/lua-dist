210ca61b63145f7e299444f47e36cd8742b2bc4a
Christopher Moore <christopher.e.moore@gmail.com>
2023-12-16 10:20:11 +0700

    using package.searchpath to find the cwd when using -ldist

--- dist.lua
+++ dist.lua
@@ -1,3 +1,8 @@
 --shortcut for -ldist
-dofile(os.getenv'LUA_PROJECT_PATH'..'/dist/run.lua')
+-- if we did use lua -ldist, then in order to determine where 'dist' is, we must search through package.path
+local path = require 'ext.path'
+local fn = package.searchpath('dist', package.path)
+fn = fn:gsub('\\', '/')
+local dir = path(fn):getdir()
+dofile(dir..'/run.lua')
 os.exit()
